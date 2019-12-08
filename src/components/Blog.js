62297fced89d9f250b3a19c8ee4b0049ae0fbc2c
Akbar <akbarali1klr@gmail.com>
2019-12-08 22:36:37 +0530

    5.6* Blog list frontend, step6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+const Details = ({url, user, likes, isVisible}) => {
+    if (isVisible) {
+        const {name=''} = user;
+        return (
+            <div>
+                <div><a href={url}>{url}</a></div>
+                <div>{likes} likes <button>Like</button></div>
+                <div>added by {name}</div>
+            </div>
+        )
+    }
+    return null;
 
+}
+const Blog = ({ blog }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    return (
+        <div style={blogStyle}>
+            <div onClick={() => setIsVisible(!isVisible)}>
+                {blog.title} {blog.author}
+            </div>
+            <Details url={blog.url} user={blog.user || {}} likes={blog.likes} isVisible={isVisible}/>
+        </div>
+    )
+}
 export default Blog
