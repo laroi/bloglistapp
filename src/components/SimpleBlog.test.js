f8445427c413ce0073e415d7d4d1c419c8c7f960
Akbar <akbarali1klr@gmail.com>
2019-12-11 00:46:18 +0530

    5.14: Blog list tests, step2

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SimpleBlog from './SimpleBlog';
 
 test('renders content', () => {
@@ -25,5 +25,22 @@ test('renders content', () => {
     expect(component.container).toHaveTextContent(
         'blog has 10 likeslike'
     );
+
+});
+test('clicking the button fires event handler', () => {
+    const blog = {
+        title: 'Test title',
+        author: 'test author',
+        likes: 10,
+    };
+    const mockHandler = jest.fn()
+    const component = render(
+        <SimpleBlog blog={blog} onClick={mockHandler} />
+    );
+    const button = component.container.querySelector('.btn')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 
 });
